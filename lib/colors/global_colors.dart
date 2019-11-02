94dec9c34fbae6381dc108581cc8c756ebaf6d80
Bruno <bruno.diaz.u@mail.pucv.cl>
2019-11-02 02:04:14 -0300

    LoginPage

---
+++ lib/colors/global_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+final Color loginBackgroundColor = new Color(0xFAFAFA);
+
+final Color loginEmailColor = Colors.red;
+final Color loginPasswordColor = Colors.red;
+final Color loginButton = Colors.lightGreen;
+final Color loginGoogleButton = Colors.red;
+final Color loginGoogleIcon = Colors.white;
+final Color loginFacebookButton = new Color(0x29487D);
+final Color loginFacebookIcon = Colors.white;
---
+++ lib/styles/text_styles.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+final TextStyle loginMessageTitle = new TextStyle(fontFamily: 'Awaita', fontSize: 40, fontWeight: FontWeight.bold, color: Colors.white, );
+final TextStyle loginMessageSubTitle = TextStyle(fontFamily: 'Awaita', fontSize: 20, fontWeight: FontWeight.normal, color: Colors.white60);
+
+final TextStyle loginInTo = new TextStyle(fontFamily: 'Awaita', fontSize: 30, fontWeight: FontWeight.bold);
+final TextStyle startMessage = new TextStyle(fontFamily: 'Awaita', fontSize: 30, fontWeight: FontWeight.bold, color: Colors.redAccent);
+
+final TextStyle hintEmailLogin = new TextStyle(fontFamily: 'Awaita', fontSize: 14, fontWeight: FontWeight.normal, color: Colors.grey);
+final TextStyle hintPasswordLogin = new TextStyle(fontFamily: 'Awaita', fontSize: 14, fontWeight: FontWeight.normal, color: Colors.grey);
+
+final TextStyle buttonLogin = TextStyle(fontFamily: 'Awaita', fontSize: 14, fontWeight: FontWeight.normal, color: Colors.white);
+final TextStyle buttonGoogleLogin = TextStyle(fontFamily: 'Awaita', fontSize: 14, fontWeight: FontWeight.normal, color: Colors.white);
