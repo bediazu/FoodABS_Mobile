53acde4042ea27677f657b3bc3edea15976e804a
Bruno <bruno.diaz.u@mail.pucv.cl>
2019-11-03 21:06:46 -0300

    Profile Page

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ A new Flutter project.
 ## Third party library
 
 - [FontAwesome Flutter](https://pub.dev/packages/font_awesome_flutter)
+- [Bubble Tab Indicator](https://pub.dev/packages/bubble_tab_indicator)
 - [Curved Navigation Bar](https://pub.dev/packages/curved_navigation_bar)
 - [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
 
--- lib/styles/text_styles.dart
+++ lib/styles/text_styles.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+/* de momento los dejare globales, no tengo una paleta de colores o algo,
+  van saliendo del paso, asi que cambia los colores si quieres, da igual */
+
 final TextStyle loginMessageTitle = new TextStyle(
   fontFamily: 'Awaita',
   fontSize: 40,
@@ -36,8 +39,33 @@ final TextStyle buttonLogin = TextStyle(
     fontSize: 14,
     fontWeight: FontWeight.normal,
     color: Colors.white);
+
 final TextStyle buttonGoogleLogin = TextStyle(
     fontFamily: 'Awaita',
     fontSize: 14,
     fontWeight: FontWeight.normal,
     color: Colors.white);
+
+TextStyle nameTitleProfile = TextStyle(
+    fontFamily: 'Awaita',
+    fontSize: 28,
+    fontWeight: FontWeight.normal,
+    color: Colors.black38);
+
+TextStyle itemTabBar = TextStyle(
+    fontFamily: 'Awaita',
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+    color: Colors.black38);
+
+TextStyle itemHeight = TextStyle(
+    fontFamily: 'Awaita',
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+    color: Colors.blueGrey);
+
+TextStyle hintHeight = TextStyle(
+    fontFamily: 'Awaita',
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+    color: Colors.blueGrey);
